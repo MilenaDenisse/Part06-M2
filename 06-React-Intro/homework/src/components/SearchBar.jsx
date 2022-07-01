6ef412ea700748654648357d757929cfc15e540f
MilenaDenisse <milecamacho17@hotmail.com>
2022-07-01 17:54:44 -0300

    complete

--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-export default function SearchBar(props) {
+export default function SearchBar({onSearch}) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <input type="text" />
+      <button onClick={() => onSearch('Agregando ciudad...')}>Agregar</button>
+    </div>
+  )
 };
+
+// Si pusiera...
+// <button onClick={onSearch('Agregando ciudad...')}>Agregar</button>
+// Automaticamente al cargar la pagina se ejecturaria esa funcion, lo que yo quiero es que al ingresar algo 
+// en el campo de texto y haga click en "agregar", surja esa alerta.
